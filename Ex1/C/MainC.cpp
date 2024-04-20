9dec9e26240447c060ab0f3fb8c8fcad27803a8b
Lucas Oliveira Silva <127321390+Lucas-O-S@users.noreply.github.com>
2024-04-20 16:41:19 -0300

    Update MainC.cpp

--- Ex1/C/MainC.cpp
+++ Ex1/C/MainC.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
-
+//Teste de pilha
 int main(){
 
+    //Teste para criar pilha de int
     Pilha<int> pilha;
 
+    //teste de push
     pilha.push(20);
     pilha.push(19);
     pilha.push(24);
@@ -15,17 +17,21 @@ int main(){
     pilha.push(1);
     pilha.push(2);
 
+    //teste para ver se topo equivale ao ultimo valor adicionado
     cout << pilha.Top() << endl;
-    
+
+    //Teste para ver se é recuperado o menor valor
     cout << pilha.Mim() << endl;
 
+    //Teste Pop
     pilha.pop();
     pilha.pop();
     pilha.pop();
 
-
+    //Verificar se o topo foi alterado
     cout << pilha.Top() << endl;
-    
+
+    //Verifica se o menor valor está correto segundo a logica
     cout << pilha.Mim() << endl;
 
     return 0;
